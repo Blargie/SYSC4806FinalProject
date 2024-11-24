329cccce89d201c96967fe2d4979a0f99b808f3e
Lucas Whitaker <lucaswhitaker22@gmail.com>
2024-11-24 14:22:15 -0500

    Updated answer-survey to work w controller changes

--- src/main/java/project/survey/SurveyController.java
+++ src/main/java/project/survey/SurveyController.java
@@ -1,22 +1,29 @@
 package project.survey;
 
-import org.springframework.web.bind.annotation.*;
-import project.answer.Answer;
-import project.answer.TextAnswer;
-import project.answer.MultipleChoiceAnswer;
-import project.answer.NumericRangeAnswer;
-import project.answer.AnswerRepository;
-import project.question.Question;
-import project.question.QuestionRepository;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Date;
+import project.answer.Answer;
+import project.answer.AnswerRepository;
+import project.answer.MultipleChoiceAnswer;
+import project.answer.NumericRangeAnswer;
+import project.answer.TextAnswer;
+import project.question.Question;
+import project.question.QuestionRepository;
 
 @Controller
 @RequestMapping("/api/surveys")
@@ -178,10 +185,10 @@ public class SurveyController {
     }
 
     // handles submitting survey answers
-    @PostMapping("/{surveyId}/submit")
-    public ResponseEntity<String> submitSurveyAnswers(@PathVariable Integer surveyId,
-            @RequestBody Map<String, String> answers,
-            @RequestParam(required = false) Integer userId) {
+    @PostMapping(value = "/{surveyId}/submit", consumes = MediaType.APPLICATION_JSON_VALUE)
+public ResponseEntity<String> submitSurveyAnswers(@PathVariable Integer surveyId,
+        @RequestBody Map<String, String> answers,
+        @RequestParam(required = false) Integer userId) {
         Survey survey = surveyRepository.findById(surveyId)
                 .orElseThrow(() -> new IllegalArgumentException("Survey not found"));
 
